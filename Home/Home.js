7eb565611788a40e55bcd34a487583bb6f9283b5
Ayush Mazumdar <ayush.mazumdar111@gmail.com>
2024-06-22 23:38:38 +0530

    Added a toggle button for Light/Dark theme in home page

--- Home/Home.js
+++ Home/Home.js
@@ -1,11 +1,63 @@
-document.getElementById("TempConvertor").onclick=function(){
+const TempConvertor=document.getElementById("TempConvertor");
+const Stopwatch=document.getElementById("Stopwatch");
+const NumberGuess=document.getElementById("NumberGuess");
+
+const arrApp=[TempConvertor,Stopwatch,NumberGuess];
+
+document.body.style.backgroundColor="black";
+document.body.style.color="white";
+
+arrApp.forEach(function(id){
+    id.style.backgroundColor="black";
+    id.style.color="white";
+    id.onmouseenter=function(){id.style.backgroundColor="white";id.style.color="black";}
+    id.onmouseleave=function(){id.style.backgroundColor="black";id.style.color="white";}
+});
+
+
+function lighttheme(){
+    arrApp.forEach(function(id){
+        id.style.backgroundColor="white";
+        id.style.color="black";
+        id.onmouseenter=function(){id.style.backgroundColor="black";id.style.color="white";}
+        id.onmouseleave=function(){id.style.backgroundColor="white";id.style.color="black";}
+    });
+}
+function darktheme(){
+    arrApp.forEach(function(id){
+        id.style.backgroundColor="black";
+        id.style.color="white";
+        id.onmouseenter=function(){id.style.backgroundColor="white";id.style.color="black";}
+        id.onmouseleave=function(){id.style.backgroundColor="black";id.style.color="white";}
+    });
+}
+
+const theme=document.getElementById("theme")
+let themeDark=true;
+theme.onclick=function(){
+    if(themeDark==true){
+        document.body.style.backgroundColor="white";
+        document.body.style.color="black";
+        lighttheme();
+        themeDark=false;
+    }
+    else{
+        document.body.style.backgroundColor="black";
+        document.body.style.color="white";
+        darktheme();
+        themeDark=true;
+    }
+}
+
+
+TempConvertor.onclick=function(){
     location.href="../TempConvertor/TempConvertor.html"
 }
 
-document.getElementById("Stopwatch").onclick=function(){
+Stopwatch.onclick=function(){
     location.href="../Stopwatch/stopwatch.html"
 }
 
-document.getElementById("NumberGuess").onclick=function(){
+NumberGuess.onclick=function(){
     location.href="../NumberGuess/NumberGuess.html"
 }
