3c8db73c6b2651b4296d5110ff1b912a074a8c19
Ayush Mazumdar <ayush.mazumdar111@gmail.com>
2024-06-22 21:00:39 +0530

    Addded Stopwatch

---
+++ Stopwatch/stopwatch.js
@@ -0,0 +1,44 @@
+let hrs;
+let min;
+let sec;
+let mls;
+let timer;
+let temp;
+const start=document.getElementById("start");
+const stop=document.getElementById("stop");
+const reset=document.getElementById("reset");
+
+function initialize(){
+    hrs=0;
+    min=0;
+    sec=0;
+    mls=0;
+    document.getElementById("time").textContent=`00:00:00:00`;
+}
+
+function display(){
+    stop.onclick=() => clearInterval(timer);
+    reset.onclick=initialize;
+    mls++;
+    if(mls==65){
+        mls=0;
+        sec++;
+    }
+    if(sec==60){
+        sec=0;
+        min++;
+    }
+    if(min==60){
+        min=0;
+        hrs++;
+    }
+    document.getElementById("time").textContent=`${hrs<10?`0${hrs}`:hrs}:${min<10?`0${min}`:min}:${sec<10?`0${sec}`:sec}:${mls}`;
+    timer=setTimeout(display,1);
+}
+
+initialize();
+start.onclick=display;
+
+
+
+
