f0b501e4c08e6b4093264ae1fa312e72f2bfb056
Ayush Mazumdar <ayush.mazumdar111@gmail.com>
2024-06-23 17:28:42 +0530

    Improved the toggle button

--- Home/Home.js
+++ Home/Home.js
@@ -33,22 +33,37 @@ function darktheme(){
 }
 
 const theme=document.getElementById("theme")
+
+theme.style.backgroundColor="black";
+theme.textContent="🌙";
+theme.onmouseenter=function(){theme.style.backgroundColor="white";theme.textContent="🔆";}
+theme.onmouseleave=function(){theme.style.backgroundColor="black";theme.textContent="🌙";}
+
 let themeDark=true;
 theme.onclick=function(){
     if(themeDark==true){
+        theme.style.backgroundColor="white";
+        theme.textContent="🔆";
+        theme.onmouseenter=function(){theme.style.backgroundColor="black";theme.textContent="🌙";}
+        theme.onmouseleave=function(){theme.style.backgroundColor="white";theme.textContent="🔆";}
+        
         document.body.style.backgroundColor="white";
         document.body.style.color="black";
         lighttheme();
         themeDark=false;
     }
     else{
+        theme.style.backgroundColor="black";
+        theme.textContent="🌙";
+        theme.onmouseenter=function(){theme.style.backgroundColor="white";theme.textContent="🔆";}
+        theme.onmouseleave=function(){theme.style.backgroundColor="black";theme.textContent="🌙";}
+
         document.body.style.backgroundColor="black";
         document.body.style.color="white";
         darktheme();
         themeDark=true;
     }
 }
-
 
 TempConvertor.onclick=function(){
     location.href="../TempConvertor/TempConvertor.html"
